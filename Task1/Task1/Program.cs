74909210a9e536165efcd63068985784e835a8bd
Krum Babachev <44364634+krumbabachev@users.noreply.github.com>
2018-12-03 11:54:12 +0200

    Add files via upload

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2091 to binary : {0}", Convert.ToString(2091,2));
+            int deci = 2091;
+            Console.WriteLine("{0} to hexadecimal is : {1}",deci, deci.ToString("X"));
+        }
+    }
+}
---
+++ Task37-7/Task37-7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task37_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double d;
+            //Console.Write("d = ");
+            //d = double.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //double t;
+            //Console.Write("d = ");
+            //t = double.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //double q;
+            //Console.Write("d = ");
+            //q = double.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //double s;
+            //Console.Write("d = ");
+            //s = double.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //double v;
+            //Console.Write("d = ");
+            //v = double.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //double r;
+            //Console.Write("d = ");
+            //r = double.Parse(Console.ReadLine());
+            //Console.WriteLine();
+
+            //(d = t | d !=t&s + d>5*r)|!(q*v!v)|q&&v
+
+
+            Console.WriteLine("(d = t || d != t && s + d > 5*r)&&!(q || ! v)||q&&v");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
